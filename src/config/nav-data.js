3f0cbd87168158b38480a5b2473d7e81dc28377d
Marina-ts <Tkachenkoms72@gmail.com>
2022-03-19 08:44:00 +0500

    #1

--- src/config/nav-data.js
+++ src/config/nav-data.js
@@ -18,36 +18,262 @@ const navData = [
 				title: 'Городские',
 				items: [],
 			},
+			{
+				id: '4',
+				title: 'Складные',
+				items: [],
+			},
+			{
+				id: '5',
+				title: 'Двухподвесные',
+				items: [],
+			},
+			{
+				id: '6',
+				title: 'Электровелосипеды',
+				items: [],
+			},
+			{
+				id: '7',
+				title: 'Женские',
+				items: [],
+			},
+			{
+				id: '8',
+				title: 'Подростковые',
+				items: [],
+			},
+			{
+				id: '9',
+				title: 'Детские',
+				items: [],
+			},
 		]
 	},
 	{
 		id: '2',
-		title: 'Комплектующие',
+		title: 'BMX',
 		items: [
 			{
 				id: '1',
-				title: 'Колеса',
+				title: 'BMX park',
 				items: [],
 			},
 			{
 				id: '2',
-				title: 'Рамы',
+				title: 'BMX vert',
+				items: [],
+			},
+			{
+				id: '3',
+				title: 'BMX street',
 				items: [],
 			},
 		]
 	},
 	{
 		id: '3',
-		title: 'Аксессуары',
+		title: 'Запчасти для велосипедов',
 		items: [
 			{
 				id: '1',
-				title: 'Наклейки',
+				title: 'Камеры',
 				items: [],
 			},
 			{
 				id: '2',
-				title: 'Сумки',
+				title: 'Педали',
+				items: [],
+			},
+			{
+				id: '3',
+				title: 'Цепи',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '4',
+		title: 'Велоаксессуары',
+		items: [
+			{
+				id: '1',
+				title: 'Велозамки',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Звонки',
+				items: [],
+			},
+			{
+				id: '3',
+				title: 'Шлемы',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '5',
+		title: 'Самокаты',
+		items: [
+			{
+				id: '1',
+				title: 'Городские',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Трюковые',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '6',
+		title: 'Скейтборды',
+		items: [
+			{
+				id: '1',
+				title: 'Классические',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Лонгборды',
+				items: [],
+			},
+			{
+				id: '3',
+				title: 'Пенни борды',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '7',
+		title: 'Ролики ',
+		items: [
+			{
+				id: '1',
+				title: 'Для взрослых',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Для детей',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '8',
+		title: 'Рюкзаки',
+		items: [
+			{
+				id: '1',
+				title: 'Велорюкзаки',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Городские',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '9',
+		title: 'Одежда',
+		items: [
+			{
+				id: '1',
+				title: 'Штаны',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Куртки',
+				items: [],
+			},
+			{
+				id: '3',
+				title: 'Комбинезоны',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '10',
+		title: 'Сноуборд',
+		items: [
+			{
+				id: '1',
+				title: 'Фристайл',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Симметричный',
+				items: [],
+			},
+			{
+				id: '3',
+				title: 'Гибридный',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '11',
+		title: 'Лыжи',
+		items: [
+			{
+				id: '1',
+				title: 'Трассовые',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Универсальные',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '12',
+		title: 'Коньки',
+		items: [
+			{
+				id: '1',
+				title: 'Конькобежные',
+				items: [],
+			},
+			{
+				id: '2',
+				title: 'Хоккейные',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '13',
+		title: 'Ещё',
+		items: [
+			{
+				id: '1',
+				title: 'Прочее',
+				items: [],
+			},
+		]
+	},
+	{
+		id: '14',
+		title: 'SALE %',
+		items: [
+			{
+				id: '1',
+				title: 'Сезонное',
 				items: [],
 			},
 		]
